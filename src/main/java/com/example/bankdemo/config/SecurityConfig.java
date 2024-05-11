7b3c84df4a8e44c27cfd29c738b871691a1c0f16
ryoheiY <momoyamashia@icloud.com>
2024-05-11 21:35:33 +0900

    PassowordEncodeのBeanを使う設定に変更

--- src/main/java/com/example/bankdemo/config/SecurityConfig.java
+++ src/main/java/com/example/bankdemo/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -28,7 +31,7 @@ public class SecurityConfig {
      */
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
+        /**UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("12345")
                 .authorities("admin")
@@ -39,8 +42,24 @@ public class SecurityConfig {
                 .password("123456")
                 .authorities("read")
                 .build();
+         **/
+
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("123456")
+                .authorities("read")
+                .build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
